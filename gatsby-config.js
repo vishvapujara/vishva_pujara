d8af3c333c2a2fb9c17a02dc8940830bb010921c
Karan Popat <krn.ppt@gmail.com>
2023-05-24 16:07:13 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Vishva Pujara`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
